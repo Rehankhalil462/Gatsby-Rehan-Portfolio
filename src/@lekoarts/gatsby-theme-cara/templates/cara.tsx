7f4747cb14419c3770caf2230c0dc2c7840ab69a
Rehan Khalil <rehankhalil462@gmail.com>
2021-04-01 01:08:08 +0500

    stablizing

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import { Parallax } from "react-spring/renderprops-addons.cjs"
-import Layout from "../components/layout"
-import Hero from "../components/hero"
-import Projects from "../components/projects"
-import About from "../components/about"
-import Contact from "../components/contact"
+import Layout from "../layout"
+import Hero from "../hero"
+import Projects from "../projects"
+import About from "../about"
+import Contact from "../contact"
 
 const Cara = () => (
   <Layout>
